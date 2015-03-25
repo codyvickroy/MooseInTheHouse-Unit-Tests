edbafb3e8af72bb70b5de11a53e2cde865ecc9dd
Brandt Newton <btnewton@uvm.edu>
2015-03-25 13:53:30 -0400

    Finished StandardBehavior Bot tests with a very good amount of testing. Added a method to automate the turn simulating process and cap number of moves per turn.

--- models/StandardBehavior_OffenseTests.java
+++ models/StandardBehavior_OffenseTests.java
@@ -3,11 +3,7 @@ package models;
 import junit.framework.TestCase;
 import models.card.Card;
 import models.card.bottom.*;
-import models.card.top.Door;
-import models.card.top.MooseBait;
-import models.card.top.MooseInBathroom;
-import models.card.top.MooseInBedroom;
-import models.game.Deck;
+import models.card.top.*;
 import models.player.Bot;
 import models.player.Move;
 import models.player.Player;
@@ -26,22 +22,19 @@ public class StandardBehavior_OffenseTests extends TestCase {
     public void setUp() throws Exception {
         super.setUp();
 
-        // Create deck
-        Deck deck = new Deck();
-
         players = new Player[] {
                     new Bot(0, null),
                     new Bot(1, null),
                     new Bot(2, null)
         };
 
-        // Offense
+        // Hand
         ArrayList<Card> offenseBotHand = new ArrayList<Card>();
-        offenseBotHand.add(new Moose());
-        offenseBotHand.add(new MooseBait());
         offenseBotHand.add(new MooseInBedroom());
-        offenseBotHand.add(new Door());
+        offenseBotHand.add(new MooseInKitchen());
         offenseBotHand.add(new MooseInBathroom());
+        offenseBotHand.add(new MooseInBedroom());
+        offenseBotHand.add(new Moose());
 
         // Create bot with standard behavior
         offenseTestBot = new Bot(3, offenseBotHand, new StandardBehavior());
@@ -50,8 +43,9 @@ public class StandardBehavior_OffenseTests extends TestCase {
         players[0].setCardInHouse(0, new EmptyLivingRoom());
         players[0].setCardInHouse(1, new EmptyBathroom());
         players[0].setCardInHouse(2, new EmptyBedroom());
-        players[0].setCardInHouse(3, new EmptyKitchen());
-        players[0].setPoints(0);
+        players[0].setCardInHouse(3, new EmptyBathroom());
+        players[0].setCardInHouse(4, new EmptyKitchen());
+        players[0].setPoints(1);
 
         // Initialize Player 1 with HIGHEST POINTS and MOOSE
         players[1].setCardInHouse(0, new Moose());
@@ -64,7 +58,7 @@ public class StandardBehavior_OffenseTests extends TestCase {
         players[2].setCardInHouse(0, new EmptyLivingRoom());
         players[2].setCardInHouse(1, new EmptyBathroom());
         players[2].setCardInHouse(2, new EmptyBedroom());
-        players[2].setCardInHouse(3, new EmptyKitchen());
+        players[2].setCardInHouse(3, new Moose());
         players[2].setPoints(2);
     }
 
@@ -89,12 +83,92 @@ public class StandardBehavior_OffenseTests extends TestCase {
     // =======================
     // === Offensive Moves ===
     // =======================
-    public void testFirstMove() {
-        Move move = offenseTestBot.makeMove(players);
 
-        assertEquals(move.getCard().getCardClass(), Card.CardClass.MOOSE);
-        assertEquals(move.getCardPlayerID(), 3);
-        assertEquals(move.getRecievingPlayer(), 0);
+    /**
+     * Player 0 is the first player considered due to their score. A moose is played even though it is
+     * the last card considered in the bot's hand.
+     */
+    public void testFirstMove() {
+        Move move = simulateTurnWithMoveCap(1);
+
+        assertEquals(Card.CardClass.MOOSE, move.getCard().getCardClass());
+        assertEquals(offenseTestBot.getID(), move.getCardPlayerID());
+        assertEquals(0, move.getReceivingPlayer());
+        assertEquals(5, move.getHousePosition());
     }
 
+    /**
+     * Player 0 is still the first player considered because their score has not changed. A moose is now in their house
+     * and a bedroom can now be played at position 2.
+     */
+    public void testSecondMove() {
+        Move move = simulateTurnWithMoveCap(2);
+
+        assertEquals(Card.CardClass.BEDROOM, move.getCard().getCardClass());
+        assertEquals(offenseTestBot.getID(), move.getCardPlayerID());
+        assertEquals(0, move.getReceivingPlayer());
+    }
+
+    /**
+     * Player 0 now has the same score as Player 2 but they are still considered first because of an arbitrary comparison
+     * in the sorting algorithm.
+     */
+    public void testThirdMove() {
+        Move move = simulateTurnWithMoveCap(3);
+
+        assertEquals(Card.CardClass.KITCHEN, move.getCard().getCardClass());
+        assertEquals(offenseTestBot.getID(), move.getCardPlayerID());
+        assertEquals(0, move.getReceivingPlayer());
+    }
+
+    /**
+     * Player 2 now has the lowest score and a Bathroom will now be played on them.
+     */
+    public void testFourthTurnMove() {
+        Move move = simulateTurnWithMoveCap(4);
+
+        assertEquals(Card.CardClass.BATHROOM, move.getCard().getCardClass());
+        assertEquals(offenseTestBot.getID(), move.getCardPlayerID());
+        assertEquals(2, move.getReceivingPlayer());
+    }
+
+    /**
+     * All Players have the same score: Player 0 is considered first followed by Player 1 who has an openning.
+     */
+    public void testFifthTurnMove() {
+        Move move = simulateTurnWithMoveCap(5);
+
+        assertEquals(Card.CardClass.BEDROOM, move.getCard().getCardClass());
+        assertEquals(offenseTestBot.getID(), move.getCardPlayerID());
+        assertEquals(1, move.getReceivingPlayer());
+    }
+
+    /**
+     * Bot has no more valid moves (Empty hand condition) so it returns a null move.
+     */
+    public void testFinalTurnMove() {
+        Move move = simulateTurnWithMoveCap(6);
+
+        assertNull(move);
+    }
+
+    /**
+     * Simulate a bot's turn.
+     *
+     * @param moves number of moves to simulate
+     * @return      last move made
+     */
+    public Move simulateTurnWithMoveCap(int moves) {
+
+        Move move = null;
+
+        for (int i = 0; i < moves; i++) {
+            move = offenseTestBot.makeMove(players);
+            if (move != null) {
+                players[move.getReceivingPlayer()].setCardInHouse(move);
+            }
+        }
+
+        return move;
+    }
 }
